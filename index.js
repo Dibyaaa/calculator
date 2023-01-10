20251cdb240d83d07b4509608e80b1ada378750e
Dibyaaa <120543129+Dibyaaa@users.noreply.github.com>
2023-01-10 14:11:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+let result = document.getElementById("inputext");
+
+let calculate = (number) => {
+    result.value += number;
+
+}
+
+let Result = () => {
+    try {
+
+        result.value = eval(result.value)
+    } catch (err) {
+        alert("ENTER THE VALID INPUT");
+
+
+    }
+}
+
+function clr() {
+    result.value = " ";
+}
+
+function del() {
+    result.value = result.value.slice(0, -1);
+}
